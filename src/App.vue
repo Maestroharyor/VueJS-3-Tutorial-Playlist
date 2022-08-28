5f4d893d3d62b9871ff94ba9c92608e2e1e7ef43
maestroharyor <Haryor1josh@gmail.com>
2022-08-28 22:58:21 +0100

    Lesson 12

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template lang="html">
   <DefaultLayout>
-    <p>Welcome to Homepage</p>
-    <font-awesome-icon icon="fa-solid fa-user-secret" />
-    <font-awesome-icon icon="fa-solid fa-user" />
-    <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+    <div class="p-10 flex flex-col gap-5 items-center justify-center">
+      <p class="text-3xl">Welcome to {{ name }}</p>
+
+      <input type="text"
+        class="text-2xl p-5 border rounded"
+        :value="name">
+
+      <a :href="url">My Link</a>
+
+
+
+      <button @click="changeName"
+        class="bg-green-500 text-white px-8 py-2 rounded hover:bg-green-600 transition">Change Name</button>
+      <button @click="changeLink"
+        class="bg-orange-500 text-white px-8 py-2 rounded hover:bg-orange-600 transition">Change Link</button>
+    </div>
+
+
   </DefaultLayout>
 
 </template>
@@ -16,6 +30,26 @@ export default {
   name: 'App',
   components: {
     DefaultLayout
+  },
+  data() {
+    return {
+      name: "Lifetechfacts",
+      url: "https://thelifetechfacts.com"
+    }
+  },
+  methods: {
+    changeName() {
+      if (this.name.toLowerCase() === "lifetechfacts") {
+        this.name = "Maestro"
+      } else {
+        this.name = "Lifetechfacts"
+      }
+
+    },
+    changeLink() {
+      this.url = "https://google.com"
+
+    },
   },
 
 }
