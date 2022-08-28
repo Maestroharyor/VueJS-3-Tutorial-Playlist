c806c586ab7c934f0e428ddedbaab584871d763a
maestroharyor <Haryor1josh@gmail.com>
2022-08-28 12:46:56 +0100

    Lesson 10

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "@fontsource/koho/200.css";
+import "@fontsource/koho/300.css";
+import "@fontsource/koho/400.css";
+import "@fontsource/koho/500.css";
+import "@fontsource/koho/600.css";
+import "@fontsource/koho/700.css";
 import "./assets/global.css";
 
 createApp(App).mount("#app");
